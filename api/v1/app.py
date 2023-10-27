72f9dee44a7179eaa8c33efcdeae4eddef45b65b
Benard Ronoh <biikate48@gmail.com>
2023-10-27 16:44:30 +0300

    Swagger, CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,16 +6,20 @@ for version 1 of our clone
 """
 
 # Importing libraries and modules
-from flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
+from os import environ
+from flask import Flask, render_template, make_response, jsonify
+from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 
 # Creating an instance of Flask
 app = Flask(__name__)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -36,8 +40,20 @@ def not_found(error):
     """
     return make_response(jsonify({'error': "Not found"}), 404)
 
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API',
+    'uiversion': 3
+}
+
+Swagger(app)
+
 
 if __name__ == "__main__":
-    host = os.environ.get("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.environ.get("HBNB_API_PORT", 5000))
+    """ Main Function """
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
