7f9d058b01b1ce0f8faf6490f0c10b07949eaf87
Kevin_Koech <kevinkoech@proton.me>
2023-10-27 15:28:25 +0300

    create a status route

---
+++ api/v1/views/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', strict_slashes=False)
+def check_status():
+    """
+    Check status of API
+    """
+    return jsonify(
+        {
+            "status": "OK"
+        }
+    ), 200
