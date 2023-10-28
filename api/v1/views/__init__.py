18acd25be3c98dec5b8f19e653541d3586c2406b
Benard Ronoh <biikate48@gmail.com>
2023-10-28 21:03:43 +0300

    fixing imports in init file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,9 +5,8 @@ Create a Blueprint for views
 """
 
 from flask import Blueprint
-
-app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
+
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
